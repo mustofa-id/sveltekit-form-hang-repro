3818321dcd7a81ad38f227025f16d7ff55f145d1
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2024-03-15 10:16:46 +0700

    chore: add description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <form method="post" use:enhance>
   <section>
-    {#each { length: 200 } as _, i}
+    {#each { length: 167 } as _, i}
       <label>
         <input type="checkbox" name="item" value="item-{i}" checked />
         <span>Item {i}</span>
@@ -22,3 +22,22 @@
     {/if}
   </section>
 </form>
+
+<p>
+  Open the Network tab in DevTools, then try clicking the Submit button. You'll
+  notice that the request is pending or hangs.
+</p>
+
+<p>
+  Reload the page, uncheck the last item and then try submitting again. It will
+  succeed.
+</p>
+
+<p>
+  It seems to hang if the content-length is <a
+    href="https://github.com/sveltejs/kit/issues/11126#issuecomment-1977218146"
+  >
+    above 16 KiB
+  </a>
+  and using <code>:enhance</code>.
+</p>
